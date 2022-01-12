ae40a3c0149144f1ac3e77fc195c425d0da3950a
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-01-12 13:26:21 -0500

    build(rtl): configure linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint",
-  ],
-  "extends": [
+  plugins: ["@typescript-eslint", "testing-library", "jest-dom"],
+  extends: [
     "next/core-web-vitals",
     "plugin:storybook/recommended",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier"
-  ]
+    "prettier",
+    "plugin:testing-library/react",
+    "plugin:jest-dom/recommended",
+  ],
+  rules: {},
 }
