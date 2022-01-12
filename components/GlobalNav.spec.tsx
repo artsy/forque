a2d3d1e698a6ca143d482026797b24e47da1074f
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-01-12 13:26:17 -0500

    feat(rtl): add sample Jest+RTL spec

---
+++ components/GlobalNav.spec.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+
+import { GlobalNav } from "./GlobalNav"
+
+describe("logged-out user", () => {
+  it("renders a login option ", () => {
+    const user = null
+
+    render(<GlobalNav user={user} />)
+
+    expect(screen.getByRole("link", { name: "Login" })).toBeVisible()
+  })
+})
+
+describe("logged-in user", () => {
+  it("renders a logout option ", () => {
+    const user = { email: "fake@artsymail.com" }
+
+    render(<GlobalNav user={user} />)
+
+    expect(screen.getByRole("link", { name: "Logout" })).toBeVisible()
+  })
+})
