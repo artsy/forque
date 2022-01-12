3dd5f55c9f024c80652427447c33cee333a555db
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-01-12 13:25:24 -0500

    build(eslint): configure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+  ],
   "extends": [
     "next/core-web-vitals",
-    "plugin:storybook/recommended"
+    "plugin:storybook/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
   ]
 }
