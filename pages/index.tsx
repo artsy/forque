2f010f70f16666cdf3e05cf395ea98914b2236a4
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-01-14 08:26:15 -0500

    feat: pikachu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next"
 import Head from "next/head"
+import Image from "next/image"
 
 import { useUser } from "../lib/artsy-next-auth"
 
@@ -14,15 +15,20 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-        <h1 className="text-xxl">Welcome to Artsy</h1>
-        <p className="my-1 p-1 bg-blue10">Hi there 👋🏽 {user.email}</p>
-        <p className="my-1">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum
-          reiciendis corrupti veniam quidem ea adipisci excepturi iure ad,
-          laboriosam soluta! Dolorum impedit qui quidem debitis aut ratione
-          culpa atque officia!
+      <div className="flex flex-col items-center mt-4 sm:mt-12">
+        <h1 className="mb-4 text-xxl">Welcome to Artsy</h1>
+        <p className="mb-4">
+          Hi, <strong>{user.email}</strong>
         </p>
+        <div className="relative [width:20em] aspect-[196/129]">
+          <Image
+            src="/under.gif"
+            width={196}
+            height={129}
+            layout="fill"
+            alt="Pikachu jackhammering in front of an Under Construction sign"
+          />
+        </div>
       </div>
     </div>
   )
