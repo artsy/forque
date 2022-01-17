8c6c913bd2d275599591634271b0266f2792088a
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-01-17 17:15:59 -0500

    build(rtl): setup @testing-library/jest-dom

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/cypress"
-  ],
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["/node_modules/", "/cypress"],
   globals: {
     "ts-jest": {
-      tsconfig: "tsconfig.jest.json"
-    }
-  }
-};
+      tsconfig: "tsconfig.jest.json",
+    },
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
+}
