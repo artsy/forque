5fc5f035d1883957b425e0352cbbcaa256a929c2
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-03-25 18:08:27 -0400

    feat: expand debug pages to show publicRuntimeConfig

--- src/pages/env/index.page.tsx
+++ src/pages/env/index.page.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from "next"
 import Head from "next/head"
+import getConfig from "next/config"
+
+const { publicRuntimeConfig } = getConfig()
 
 const Home: NextPage = () => {
   return (
@@ -11,15 +14,21 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="flex flex-col items-center mt-4 sm:mt-12">
-        <h1 className="mb-4 text-xxl">Hello, Env</h1>
-        GRAVITY_URL: {process.env.GRAVITY_URL}
+        <h1 className="text-xxl">Hello, Env</h1>
+        <h2 className="mt-4 mb-2 text-lg">process.env</h2>
+        GRAVITY_URL: ⚠️ {process.env.GRAVITY_URL} (not expected)
         <br />
-        METAPHYSICS_URL: {process.env.METAPHYSICS_URL}
+        METAPHYSICS_URL: ⚠️ {process.env.METAPHYSICS_URL} (not expected)
         <br />
         NEXT_PUBLIC_GRAVITY_URL: {process.env.NEXT_PUBLIC_GRAVITY_URL}
         <br />
         NEXT_PUBLIC_METAPHYSICS_URL: {process.env.NEXT_PUBLIC_METAPHYSICS_URL}
         <br />
+        <h2 className="mt-4 mb-2 text-lg">publicRuntimeConfig</h2>
+        NEXT_PUBLIC_GRAVITY_URL: {publicRuntimeConfig.NEXT_PUBLIC_GRAVITY_URL}
+        <br />
+        NEXT_PUBLIC_METAPHYSICS_URL:{" "}
+        {publicRuntimeConfig.NEXT_PUBLIC_METAPHYSICS_URL}
       </div>
     </div>
   )
--- src/pages/env/server.page.tsx
+++ src/pages/env/server.page.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from "next"
 import Head from "next/head"
+import getConfig from "next/config"
+
+const { publicRuntimeConfig } = getConfig()
 
 const Home: NextPage = (props) => {
   return (
@@ -11,18 +14,24 @@ const Home: NextPage = (props) => {
       </Head>
 
       <div className="flex flex-col items-center mt-4 sm:mt-12">
-        <h1 className="mb-4 text-xxl">Hello, Env (server)</h1>
-        GRAVITY_URL: {process.env.GRAVITY_URL}
+        <h1 className="text-xxl">Hello, Env (server)</h1>
+        <h2 className="mt-4 mb-2 text-lg">process.env</h2>
+        GRAVITY_URL: ⚠️ {process.env.GRAVITY_URL} (not expected)
         <br />
-        METAPHYSICS_URL: {process.env.METAPHYSICS_URL}
+        METAPHYSICS_URL: ⚠️ {process.env.METAPHYSICS_URL} (not expected)
         <br />
         NEXT_PUBLIC_GRAVITY_URL: {process.env.NEXT_PUBLIC_GRAVITY_URL}
         <br />
         NEXT_PUBLIC_METAPHYSICS_URL: {process.env.NEXT_PUBLIC_METAPHYSICS_URL}
         <br />
+        <h2 className="mt-4 mb-2 text-lg">publicRuntimeConfig</h2>
+        NEXT_PUBLIC_GRAVITY_URL: {publicRuntimeConfig.NEXT_PUBLIC_GRAVITY_URL}
+        <br />
+        NEXT_PUBLIC_METAPHYSICS_URL:{" "}
+        {publicRuntimeConfig.NEXT_PUBLIC_METAPHYSICS_URL}
+        <h2 className="mt-4 mb-2 text-lg">serverSideProps</h2>
+        <pre>{JSON.stringify(props)}</pre>
       </div>
-
-      <pre>{JSON.stringify(props)}</pre>
     </div>
   )
 }
