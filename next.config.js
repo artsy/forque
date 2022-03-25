ddc1c0e2d0cd900fde795c9bc1b9cb9135e8a551
Joey Aghion <joey@aghion.com>
2022-03-25 17:31:22 -0400

    fix: expose runtime config to client (#17)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_GRAVITY_URL: process.env.NEXT_PUBLIC_GRAVITY_URL,
+    NEXT_PUBLIC_METAPHYSICS_URL: process.env.NEXT_PUBLIC_METAPHYSICS_URL,
+  },
   compiler: {
     styledComponents: true
   },
