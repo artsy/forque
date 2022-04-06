2aafb64ca7516180d37bfd77dc2f55f8b65cb14f
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-04-06 01:45:24 -0400

    chore: remove obsolete getUserFromCookie()

--- src/system/artsy-next-auth/index.ts
+++ src/system/artsy-next-auth/index.ts
@@ -1,16 +1,7 @@
-import type { GetServerSidePropsContext } from "next"
 import type { User } from "next-auth"
 
 // data fetching
 export { useGravity } from "./hooks/gravity"
 export { useMetaphysics } from "./hooks/metaphysics"
-
-// temp, till we wire up next-auth
-export function getUserFromCookie(_req: GetServerSidePropsContext["req"]) {
-  return {
-    email: "test@example.com",
-    accessToken: "omglmfao",
-  }
-}
 
 export type UserWithAccessToken = User & { accessToken: string }
