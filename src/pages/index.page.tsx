38af4a128107f43268a9509651dbe3be0e4b4484
Matt Jones <matt.jones@artsymail.com>
2022-04-27 14:04:34 -0400

    chore: add sentry

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -32,6 +32,16 @@ export default function Home({ me }: HomeProps) {
             alt="Pikachu jackhammering in front of an Under Construction sign"
           />
         </div>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              throw new Error("Sentry Frontend Error")
+            }}
+          >
+            Throw error
+          </button>
+        </div>
       </div>
     </div>
   )
