e5504188f45bbb4552ee6b4c0c5dea722dc1a3d2
Matt Jones <matt.jones@artsymail.com>
2022-05-03 16:00:20 -0400

    fix: reenable sentryWebpackPlugin

--- next.config.js
+++ next.config.js
@@ -18,11 +18,7 @@ const nextConfig = {
     NEXT_PUBLIC_GRAVITY_URL: process.env.NEXT_PUBLIC_GRAVITY_URL,
     NEXT_PUBLIC_METAPHYSICS_URL: process.env.NEXT_PUBLIC_METAPHYSICS_URL,
   },
-  reactStrictMode: true,
-  sentry: {
-    disableServerWebpackPlugin: true,
-    disableClientWebpackPlugin: true,
-  }
+  reactStrictMode: true
 }
 
 const sentryWebpackPluginOptions = {
@@ -32,6 +28,8 @@ const sentryWebpackPluginOptions = {
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
 
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
@@ -39,4 +37,4 @@ const sentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(nextConfig);
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
