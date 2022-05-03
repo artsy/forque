7b8fd29e2b1a2bea47994fa0b350b96b263b71bc
Matt Jones <matt.jones@artsymail.com>
2022-05-03 16:06:42 -0400

    fix: add comment

--- next.config.js
+++ next.config.js
@@ -28,6 +28,7 @@ const sentryWebpackPluginOptions = {
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
 
+  // necesary to use authenticat sentry-cli
   authToken: process.env.SENTRY_AUTH_TOKEN,
 
   silent: true, // Suppresses all logs
