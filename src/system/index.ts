a9355675fb63bd9ed3c0971ba8cb02d1556c0005
Matt Jones <matt.jones@artsymail.com>
2022-05-20 17:09:23 -0400

    refactor: use camelCase

--- src/system/index.ts
+++ src/system/index.ts
@@ -8,10 +8,10 @@ export type UserWithAccessToken = User & {
 // all supported roles
 // NOTE: Do not add Admin to supported roles. It is to be deprectated.
 export enum Role {
-  customer_support = "customer_support",
-  metadata_admin = "metadata_admin",
+  customerSupport = "customer_support",
+  metadataAdmin = "metadata_admin",
   team = "team",
-  content_manager = "content_manager",
+  contentManager = "content_manager",
 }
 
 export enum Action {
@@ -25,23 +25,23 @@ export enum Action {
 // For each _domain_, a map of _actions_ to the authorized _roles_.
 const PERMISSIONS: Record<string, Record<string, Role[]>> = {
   users: {
-    [Action.list]: [Role.customer_support],
+    [Action.list]: [Role.customerSupport],
   },
   artists: {
-    [Action.dedupe]: [Role.metadata_admin],
-    [Action.list]: [Role.metadata_admin],
+    [Action.dedupe]: [Role.metadataAdmin],
+    [Action.list]: [Role.metadataAdmin],
     [Action.transfer]: [Role.team],
   },
   my_collection: {
-    [Action.transfer]: [Role.customer_support],
+    [Action.transfer]: [Role.customerSupport],
   },
   uploads: {
     [Action.list]: [Role.team],
     [Action.create]: [Role.team],
   },
   shortcuts: {
-    [Action.create]: [Role.content_manager],
-    [Action.edit]: [Role.content_manager],
+    [Action.create]: [Role.contentManager],
+    [Action.edit]: [Role.contentManager],
   },
 }
 
