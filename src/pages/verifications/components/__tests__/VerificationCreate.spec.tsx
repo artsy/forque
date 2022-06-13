d0b0d41db3a24052bc71e1f7890c998a75dbe279
Matt Jones <matt.jones@artsymail.com>
2022-06-13 17:26:05 -0400

    feat: add specs

---
+++ src/pages/verifications/components/__tests__/VerificationCreate.spec.tsx
@@ -0,0 +1,65 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { VarificationCreate } from "../VerificationCreate"
+import {
+  successResponse,
+  failureResponse,
+} from "../__fixtures__/verificationCreateMutationResponses"
+
+const mockUseToasts = jest.fn()
+
+jest.mock("@artsy/palette", () => ({
+  ...jest.requireActual("@artsy/palette"),
+  useToasts: () => mockUseToasts,
+}))
+
+const mockCreateIdentityVerification = jest.fn()
+
+jest.mock("../../mutations/useCreateIdentityVerification", () => ({
+  useCreateIdentityVerification: () => ({
+    submitMutation: mockCreateIdentityVerification,
+  }),
+}))
+
+it("shows a confirmation message when an identity verfication is created", () => {
+  mockCreateIdentityVerification.mockImplementation(() => successResponse)
+
+  const spy = jest.fn()
+  mockUseToasts.mockImplementation(() => ({
+    sendToast: spy,
+  }))
+
+  render(<VarificationCreate />)
+
+  const input = screen.getByPlaceholderText("user@example.com")
+
+  fireEvent.change(input, { target: { value: "human@user.com" } })
+
+  fireEvent.click(screen.getByRole("button"))
+
+  expect(spy).toHaveBeenCalledWith({
+    variant: "success",
+    message: "Identity verification created",
+  })
+})
+
+it("shows a confirmation message when an identity verification fails to be created", () => {
+  mockCreateIdentityVerification.mockImplementation(() => failureResponse)
+
+  const spy = jest.fn()
+  mockUseToasts.mockImplementation(() => ({
+    sendToast: spy,
+  }))
+
+  render(<VarificationCreate />)
+
+  const input = screen.getByPlaceholderText("user@example.com")
+
+  fireEvent.change(input, { target: { value: "human@user.com" } })
+
+  fireEvent.click(screen.getByRole("button"))
+
+  expect(spy).toHaveBeenCalledWith({
+    variant: "error",
+    message: "There was an error creating the identity verification",
+  })
+})
