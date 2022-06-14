d365cde1eef1919cb3febe8b62a8a327ebe85115
Jian <jian-feng@artsymail.com>
2022-06-14 14:59:34 -0400

    fix: stale relay result on identity verification search

--- src/pages/verifications/components/VerificationsList.tsx
+++ src/pages/verifications/components/VerificationsList.tsx
@@ -21,7 +21,7 @@ export const VerificationsList: React.FC = () => {
     `,
     { email },
     // otherwise relay fetches on page load, with undefined email, and pulls all data
-    { fetchPolicy: !email ? "store-only" : "store-or-network" }
+    { fetchPolicy: !email ? "store-only" : "network-only" }
   )
 
   return (
