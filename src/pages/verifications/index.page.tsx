bae71eae1415b9d450d4de787cb3da83c2eee77e
Matt Jones <38149304+mc-jones@users.noreply.github.com>
2022-06-14 14:55:07 -0400

    feat: assert permitted for verifications (#225)

--- src/pages/verifications/index.page.tsx
+++ src/pages/verifications/index.page.tsx
@@ -1,8 +1,14 @@
 import Head from "next/head"
 import { Tab, Tabs } from "@artsy/palette"
 import { VarificationCreate } from "./components/VerificationCreate"
+import { useSession } from "next-auth/react"
+import { Action, assertPermitted, UserWithAccessToken } from "system"
 
 const VerificationsPage: React.FC = () => {
+  const session = useSession()
+  const user = session.data?.user as UserWithAccessToken
+  assertPermitted(user, Action.list, "verifications")
+
   return (
     <>
       <Head>
