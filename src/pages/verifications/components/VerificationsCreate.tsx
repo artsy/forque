fd3fcd5b946b8a729c0a671ae6808010626da14a
Matt Jones <matt.jones@artsymail.com>
2022-06-21 15:53:41 -0400

    feat: add name input and send with mutation

--- src/pages/verifications/components/VerificationsCreate.tsx
+++ src/pages/verifications/components/VerificationsCreate.tsx
@@ -11,12 +11,13 @@ export const VerificationsCreate: React.FC = () => {
 
   interface InputTypes {
     email: string
+    name: string
   }
 
   return (
     <>
       <Formik<InputTypes>
-        initialValues={{ email: "" }}
+        initialValues={{ email: "", name: "" }}
         validationSchema={Yup.object().shape({
           email: Yup.string()
             .required("A user email is required")
@@ -28,6 +29,7 @@ export const VerificationsCreate: React.FC = () => {
               variables: {
                 input: {
                   email: values.email,
+                  name: values.name,
                 },
               },
               rejectIf: (res) => {
@@ -55,6 +57,16 @@ export const VerificationsCreate: React.FC = () => {
         {({ values, handleChange, errors }) => (
           <Form>
             <Input
+              placeholder="Jane Doe"
+              title="Full Name"
+              name="name"
+              type="text"
+              onChange={handleChange}
+              value={values.name}
+              error={errors.name}
+            />
+            <Spacer my={2} />
+            <Input
               placeholder="user@example.com"
               title="Email"
               name="email"
@@ -62,6 +74,7 @@ export const VerificationsCreate: React.FC = () => {
               onChange={handleChange}
               value={values.email}
               error={errors.email}
+              required={true}
             />
             <Spacer my={4} />
             <Button type="submit" width="100%">
