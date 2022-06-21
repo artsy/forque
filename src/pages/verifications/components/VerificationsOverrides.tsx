0a27735c62f710c14468a3f9b2d701d87f4c07c3
Ozzie Vasdi <29984068+ovasdi@users.noreply.github.com>
2022-06-21 17:20:41 -0400

    feat: add create override button

--- src/pages/verifications/components/VerificationsOverrides.tsx
+++ src/pages/verifications/components/VerificationsOverrides.tsx
@@ -1,5 +1,6 @@
-import { Text } from "@artsy/palette"
+import { Button, Column, GridColumns, Text, useToasts } from "@artsy/palette"
 import { Table } from "components/Table"
+import { useState } from "react"
 import type { Override } from "./types"
 
 interface VerificationsOverridesProps {
@@ -17,9 +18,21 @@ export const VerificationsOverrides: React.FC<VerificationsOverridesProps> = (
 
   return (
     <>
-      <Text variant="lg" my={1}>
-        Overrides
-      </Text>
+      <GridColumns>
+        <Column>
+          <Text variant="lg" my={1}>
+            Overrides
+          </Text>
+        </Column>
+        <Column p={1}>
+          <CreateOverrideButton
+            onCreate={async () => {
+              console.log("creating override")
+              // ShowOverrideModal to collect state and reason for override
+            }}
+          ></CreateOverrideButton>
+        </Column>
+      </GridColumns>
       <Table
         columns={[
           {
@@ -47,5 +60,39 @@ export const VerificationsOverrides: React.FC<VerificationsOverridesProps> = (
         onRowClick={onRowClick}
       />
     </>
+  )
+}
+
+const CreateOverrideButton: React.FC<{
+  onCreate: () => Promise<void>
+}> = ({ onCreate }) => {
+  const { sendToast } = useToasts()
+  const [showConfirmMessage, setShowConfirmMessage] = useState(false)
+  const [isCreating, setIsCreating] = useState(false)
+
+  return (
+    <Button
+      size="small"
+      variant="secondaryOutline"
+      width={80}
+      loading={isCreating}
+      onClick={async (event) => {
+        event.stopPropagation()
+        event.preventDefault()
+
+        setShowConfirmMessage(true)
+
+        if (showConfirmMessage) {
+          setIsCreating(true)
+          await onCreate()
+          sendToast({
+            message: "Successfully created override",
+            variant: "alert",
+          })
+        }
+      }}
+    >
+      {showConfirmMessage ? "Create?" : "Create"}
+    </Button>
   )
 }
