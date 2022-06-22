02eb09ad659ea351a83c679f6d7407731909153c
Jian <jian-feng@artsymail.com>
2022-06-22 13:23:06 -0400

    fix: snake_case -> camelCase for user id param

--- src/pages/verifications/components/VerificationsList.tsx
+++ src/pages/verifications/components/VerificationsList.tsx
@@ -9,7 +9,7 @@ export const VerificationsList: React.FC = () => {
   const [email, setEmail] = useState("")
 
   const router = useRouter()
-  const [userId, setUserId] = useState(router.query.user_id as string)
+  const [userId, setUserId] = useState(router.query.userId as string)
 
   interface InputTypes {
     emailInput: string
