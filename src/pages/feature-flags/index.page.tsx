ea14fe6adc1c3979d5710386cf042e2d1f0854f9
Matt Jones <matt.jones@artsymail.com>
2022-06-29 14:52:15 -0400

    fix: disable feature flags in staging

--- src/pages/feature-flags/index.page.tsx
+++ src/pages/feature-flags/index.page.tsx
@@ -1,7 +1,10 @@
-import { Tab, Tabs } from "@artsy/palette"
+import { Tab, Tabs, Banner } from "@artsy/palette"
 import { GetServerSideProps } from "next"
+import { useSession } from "next-auth/react"
+import getConfig from "next/config"
 import Head from "next/head"
 import { graphql } from "react-relay"
+import { assertPermitted, UserWithAccessToken } from "system"
 import { fetchRelayData } from "system/relay"
 import { featureFlagsQuery$data } from "__generated__/featureFlagsQuery.graphql"
 import CreateFeatureFlag from "./components/CreateFeatureFlag"
@@ -12,6 +15,18 @@ interface FeatureFlagPageProps {
 }
 
 const FeatureFlagPage: React.FC<FeatureFlagPageProps> = (props) => {
+  const { publicRuntimeConfig } = getConfig()
+  const session = useSession()
+  const user = session.data?.user as UserWithAccessToken
+  assertPermitted(user, "feature_flags")
+
+  // HACK: Temprary guard to prevent taking action against production until a
+  // better solution is implemented.
+  if (publicRuntimeConfig.NEXT_PUBLIC_METAPHYSICS_URL.includes("staging")) {
+    return (
+      <Banner variant="error">Feature Flags not available on staging</Banner>
+    )
+  }
   return (
     <>
       <Head>
