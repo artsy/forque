5dd0ce63a1a0827802e65dd4aad8aa9f7d12d167
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-07-05 07:38:24 -0400

    build: enable next/image component for Artsy image domains

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require("@sentry/nextjs")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -23,9 +23,12 @@ const nextConfig = {
   sentry: {
     disableServerWebpackPlugin: true,
     disableClientWebpackPlugin: true,
-  }
+  },
+  images: {
+    domains: ["d32dm0rphc51dk.cloudfront.net", "d2v80f5yrouhh2.cloudfront.net"],
+  },
 }
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(nextConfig);
+module.exports = withSentryConfig(nextConfig)
